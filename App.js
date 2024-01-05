55e12fad213187b910489674b92f2facee488314
devpratap001 <devps1125@gmail.com>
2024-01-05 11:20:15 +0530

    basic functionality of playing and winning added

---
+++ App.js
@@ -0,0 +1,73 @@
+const express= require("express");
+const { createServer }= require("http");
+const {Server} = require("socket.io");
+
+const app= express();
+const server= createServer(app);
+const io= new Server(server);
+var rawSockets= [];
+var sockets= [];
+var userPair= [];
+
+app.use(express.static(__dirname + "/public/home"));
+app.use("/game", express.static(__dirname + "/public/game"));
+
+app.get("/", (req, res, next) => {
+    res.send("");
+});
+
+io.on("connection", (socket) => {
+    socket.on("playerJoin", (data) => {
+        socket.data= data;
+        sockets.push(socket);
+        rawSockets.push(socket);
+        if (sockets.length ==2 ){
+            userPair= [...sockets];
+            sockets= [];
+            userPair.forEach((elem) => {
+                elem.emit("matchFound", {playerOne: userPair[0].data, playerTwo: userPair[1].data, turn: "O", activePlayer: userPair[0].data});
+            })
+        }
+    })
+    socket.on("clickedData", (data) => {
+        var competetor = []; 
+        competetor= rawSockets.filter((elem) => {
+            if (elem.data.name === data.competor.name) {
+                return elem ;
+            }
+        })
+        competetor[0].emit("clickedDataServer", (data));
+    })
+    socket.on("turnChangeClient", (data) => {
+        var socketsPair= [];
+        socketsPair= rawSockets.filter((elem) => {
+            if (elem.data.name === data.playerOne.name || elem.data.name === data.playerTwo.name){
+                return elem ;
+            }
+        })
+        socketsPair.forEach((elem) => {
+            elem.emit("turnChangeServer", {
+                ...data,
+                activePlayer: data.activePlayer.name === data.playerOne.name ? data.playerTwo: data.playerOne ,
+                turn: data.turn === "O" ? "X": "O" ,
+            })
+        })
+    })
+    socket.on("reloadBoard", (datamatch) => {
+        const competetor= rawSockets.filter((elem) => {
+            if (elem.data.name === datamatch.opponent.name){
+                return elem;
+            }
+        })
+        competetor[0].emit("reloadBoardServer", {...datamatch});
+    })
+
+    socket.on("disconnect", ()=> {
+        sockets.splice(sockets.indexOf(socket.data), 1);
+        rawSockets.splice(rawSockets.indexOf(socket).data, 1);
+    })
+})
+
+server.listen(3000, () => {
+    console.log("server started at port 3000");
+})
---
+++ public/home/script.js
@@ -0,0 +1,138 @@
+const socket = io();
+
+const form = document.querySelector("#form");
+const playerName = document.querySelector("input");
+const button = document.querySelector("#form > button");
+var currentData = {};
+var winningCases= [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [1,4,7],
+    [2,5,8],
+    [3,6,9],
+    [1,5,9],
+    [3,5,7],
+]
+
+function changeTurn(user, player) {
+    if (user.name !== player.name) {
+        document.querySelectorAll(".column").forEach((elem) => {
+            elem.disabled = true;
+        })
+    } else {
+        document.querySelectorAll(".column").forEach((elem) => {
+            elem.disabled = false;
+        })
+    }
+}
+
+function checkGameOver(winner) {
+    var buttons= document.querySelectorAll(".column");
+    winningCases.forEach( caseArray =>{
+        const first= buttons[caseArray[0] -1].innerHTML;
+        const second= buttons[caseArray[1] -1].innerHTML;
+        const third= buttons[caseArray[2] -1].innerHTML;
+        if (first !== "" && second !== "" && third !== ""){
+            if (first ===second && first === third){
+                document.querySelector(".winningAlert").innerHTML= `<h2>Match won by ${winner.name}</h2>`
+                document.querySelector(".boxes").style.display= "none";
+                document.querySelector(".winningAlert").style.display= "block";
+                return
+            }
+        }
+    })
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    socket.emit("playerJoin", { name: playerName.value });
+    socket.data = { name: playerName.value };
+    playerName.value = ""
+    playerName.setAttribute("disabled", "true")
+    button.disabled = true;
+    document.querySelector(".loader").style.display = "inline-block";
+})
+
+socket.on("matchFound", (data) => {
+    document.querySelector(".container").style.display = "none";
+    document.querySelector(".game").style.display = "flex";
+    document.querySelector(".playerOne").innerHTML = data.playerOne.name;
+    document.querySelector(".signOne").innerHTML = "O"
+    document.querySelector(".playerTwo").innerHTML = data.playerTwo.name;
+    document.querySelector(".signTwo").innerHTML = "X"
+    document.querySelector(".userCardOne").classList.add("active")
+    currentData = { ...data }
+
+    changeTurn(socket.data, data.activePlayer);
+
+    document.querySelectorAll(".column").forEach((elem, index) => {
+        elem.addEventListener("click", () => {
+            elem.innerHTML = currentData.turn;
+            checkGameOver(currentData.activePlayer);
+            socket.emit("clickedData", {
+                turn: currentData.turn,
+                buttonIndex: index,
+                competor: socket.data.name === currentData.playerOne.name ? currentData.playerTwo : currentData.playerOne ,
+                currentPlayer: socket.data.name === currentData.playerOne.name ? currentData.playerOne : currentData.playerTwo
+            })
+            socket.emit("turnChangeClient", { ...currentData })
+        }, { once: true });
+    })
+    socket.on("turnChangeServer", (data) => {
+        document.querySelector(".userCardOne").classList.toggle("active")
+        document.querySelector(".userCardTwo").classList.toggle("active")
+        changeTurn(socket.data, data.activePlayer);
+        currentData = { ...data };
+    })
+    socket.on("clickedDataServer", (data) => {
+        const targetButton = document.querySelectorAll(".column")[data.buttonIndex]
+        const cloneElem = targetButton.cloneNode(true);
+        cloneElem.innerHTML = data.turn
+        targetButton.parentNode.replaceChild(cloneElem, targetButton)
+        checkGameOver(data.currentPlayer);
+    })
+
+    document.querySelector(".reloadBoard").addEventListener("click", (e) => {
+        e.preventDefault();
+        document.querySelector(".winningAlert").style.display= "none"
+        document.querySelector(".boxes").style.display= "grid"
+        socket.emit("reloadBoard", {opponent:socket.data.name === currentData.playerOne.name ? currentData.playerTwo : currentData.playerOne})
+        document.querySelectorAll(".column").forEach((elem, index) => {
+            const cloneElem = elem.cloneNode(true);
+            cloneElem.innerHTML= "";
+            elem.parentNode.replaceChild(cloneElem, elem);
+            cloneElem.addEventListener("click", () => {
+                cloneElem.innerHTML = currentData.turn;
+                checkGameOver(currentData.activePlayer);
+                socket.emit("clickedData", {
+                    turn: currentData.turn,
+                    buttonIndex: index,
+                    competor: socket.data.name === currentData.playerOne.name ? currentData.playerTwo : currentData.playerOne ,
+                    currentPlayer: socket.data.name === currentData.playerOne.name ? currentData.playerOne : currentData.playerTwo
+                })
+                socket.emit("turnChangeClient", { ...currentData })
+            }, { once: true });
+        })
+    })
+    socket.on("reloadBoardServer", () => {
+        document.querySelector(".winningAlert").style.display= "none"
+        document.querySelector(".boxes").style.display= "grid"
+        document.querySelectorAll(".column").forEach((elem, index) => {
+            const cloneElem = elem.cloneNode(true);
+            cloneElem.innerHTML= "";
+            elem.parentNode.replaceChild(cloneElem, elem);
+            cloneElem.addEventListener("click", () => {
+                cloneElem.innerHTML = currentData.turn;
+                checkGameOver(currentData.activePlayer);
+                socket.emit("clickedData", {
+                    turn: currentData.turn,
+                    buttonIndex: index,
+                    competor: socket.data.name === currentData.playerOne.name ? currentData.playerTwo : currentData.playerOne ,
+                    currentPlayer: socket.data.name === currentData.playerOne.name ? currentData.playerOne : currentData.playerTwo
+                })
+                socket.emit("turnChangeClient", { ...currentData })
+            }, { once: true });
+        })
+    })
+})
